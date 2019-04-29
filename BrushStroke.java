5bf2261be248f169df6f5d56827e7d9e1a25f14b
nmarth <nmarth2993@protonmail.com>
2019-04-28 22:03:45 -0400

    Update BrushStroke.java
    removed start mark and end mark

--- BrushStroke.java
+++ BrushStroke.java
@@ -1,37 +1,18 @@
 import java.util.ArrayList;
 
 public class BrushStroke {
-	private Mark startMark;
-	private Mark endMark;
 	private ArrayList<Mark> marks;
 
-	public BrushStroke(Mark startMark, Mark endMark) {
-		this.startMark = startMark;
-		this.endMark = endMark;
+	public BrushStroke(ArrayList<Mark> markList) {
+		marks = markList;
 	}
-
-	public Mark getStartMark() {
-		return startMark;
-	}
-
-	public void setStartMark(Mark startMark) {
-		this.startMark = startMark;
-	}
-
-	public Mark getEndMark() {
-		return endMark;
-	}
-
-	public void setEndMark(Mark endMark) {
-		this.endMark = endMark;
-	}
-
+	
 	public ArrayList<Mark> getMarks() {
 		return marks;
 	}
 
-	public void setMarks(ArrayList<Mark> marks) {
-		this.marks = marks;
+	public void setMarks(ArrayList<Mark> markList) {
+		marks = markList;
 	}
 
 }
