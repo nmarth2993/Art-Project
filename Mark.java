480670ad80f3047d59d88af0017260bcda100c98
nmarth <nmarth2993@protonmail.com>
2019-05-23 23:39:15 -0400

    Update Mark.java

--- Mark.java
+++ Mark.java
@@ -1,10 +1,14 @@
 import java.awt.*;
 
 public class Mark {
+	public final static int CIRCLE_BRUSH = 1;
+	public final static int SQUARE_BRUSH = 2;
+	public final static int TRIANGLE_BRUSH = 3;
+
+	private int brush;
 	private Point point;
 	private Color color;
 	private Dimension size;
-	private Shape brush;
 
 	public Point getPoint() {
 		return point;
@@ -34,23 +38,27 @@ public class Mark {
 		return (int) getSize().getWidth();
 	}
 
-	public Mark(Point p, Dimension d, Color c) {
+	public Mark(Point p, Dimension d, Color c, int brush) {
 		point = p;
 		size = d;
 		color = c;
+		setBrush(brush);
 	}
 
-	public Mark(int x, int y, int width, int height, Color c) {
+	public Mark(int x, int y, int width, int height, Color c, int brush) {
 		point = new Point(x, y);
 		size = new Dimension(width, height);
 		color = c;
+		setBrush(brush);
 	}
-	
-	public Shape getShape() {
+
+	public int getBrush() {
 		return brush;
 	}
-	
-	public void setShape(Shape s) {
-		brush = s;
+
+	public void setBrush(int b) {
+		if (b > 0 && b < 4) {
+			brush = b;
+		}
 	}
 }
