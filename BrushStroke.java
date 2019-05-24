e6b897ce6f2bf1d0cca61f36fac5bfbf2435f788
nmarth <nmarth2993@protonmail.com>
2019-05-23 21:51:04 -0400

    Allow empty BrushStroke
    empty BrushStrokes allow for constant adding of marks to a BrushStroke created each mouseDown

--- BrushStroke.java
+++ BrushStroke.java
@@ -3,10 +3,13 @@ import java.util.ArrayList;
 public class BrushStroke {
 	private ArrayList<Mark> marks;
 
+	public BrushStroke() {
+	}
+
 	public BrushStroke(ArrayList<Mark> markList) {
 		marks = markList;
 	}
-	
+
 	public ArrayList<Mark> getMarks() {
 		return marks;
 	}
